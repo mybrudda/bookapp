8241406b6cbc1802456666cc905a87ba540bced4
Abdul Doranai <149365096+mybrudda@users.noreply.github.com>
2025-03-22 23:57:56 +0200

    Server setup

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const router = express.Router()
+
+
+
+router.post("/register", async (req, res) => {
+    res.send("register")
+})
+
+
+router.post("/login", async (req, res) => {
+    res.send("login")
+})
+
+export default router
