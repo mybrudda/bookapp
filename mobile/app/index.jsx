4b9f2187d32c3050fc06fee0d1dcf769866b15fc
Abdul Doranai <149365096+mybrudda@users.noreply.github.com>
2025-03-31 13:37:42 +0300

    Created expo project for frontend and added styling files

---
+++ mobile/app/index.jsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import {Image} from "expo-image"
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello</Text>
+    
+    </View>
+  );
+}
