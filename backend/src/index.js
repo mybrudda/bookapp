4fce15afd987beb268ac38d2d187d0acd757373c
Abdul Doranai <149365096+mybrudda@users.noreply.github.com>
2025-03-31 13:08:01 +0300

    Added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
 import { connectDB } from "./lib/db.js"
+import cors from "cors"
 
 const app = express()
 
@@ -10,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/books", bookRoutes)
